b78bb5f39153f52c9c9ad51139a4eae2e4013468
Loic VASILE <loic.vasile@stagiaire-elan.fr>
2024-11-07 14:10:46 +0100

    Update ForumController

--- forumPlateau_V2/controller/ForumController.php
+++ forumPlateau_V2/controller/ForumController.php
@@ -5,10 +5,12 @@ namespace Controller;
 use App\Session;
 use App\AbstractController;
 use App\ControllerInterface;
+use App\DAO;
 use Model\Entities\Topic;
 use Model\Managers\CategoryManager;
 use Model\Managers\TopicManager;
 use Model\Managers\PostManager;
+use PDO;
 
 class ForumController extends AbstractController implements ControllerInterface
 {
