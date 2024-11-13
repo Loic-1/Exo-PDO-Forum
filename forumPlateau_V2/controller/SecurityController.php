27fa47a2d12bd245cf7a9e1782f93efc0cdf31cd
Loic-1 <loic.vasile130@gmail.com>
2024-11-13 16:40:51 +0100

    Add register.php Begin register()

--- forumPlateau_V2/controller/SecurityController.php
+++ forumPlateau_V2/controller/SecurityController.php
@@ -3,11 +3,38 @@ namespace Controller;
 
 use App\AbstractController;
 use App\ControllerInterface;
+use Model\Managers\UserManager;
 
 class SecurityController extends AbstractController{
     // contiendra les méthodes liées à l'authentification : register, login et logout
 
-    public function register () {}
+    public function register () {
+
+        $userManager = new UserManager();
+
+        $pseudo = filter_input(INPUT_POST, "pseudo", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $mail = filter_input(INPUT_POST, "mail", FILTER_SANITIZE_FULL_SPECIAL_CHARS, FILTER_VALIDATE_EMAIL);
+        $pass1 = filter_input(INPUT_POST, "pass1", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $pass2 = filter_input(INPUT_POST, "pass2", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        if ($pseudo && $mail && $pass1 && $pass2) {
+            // Vérifier E-mail et Pseudo uniques
+
+            // Longueur du mot de passe variable (12 comme exemple)
+            if ($pass1 === $pass2 && strlen($pass1) >= 12) {
+                $hash = password_hash($pass1, PASSWORD_DEFAULT);
+
+                $data = ["nickName" => $pseudo, "password" => $hash, "email" => $mail];
+
+                $userManager->add($data);
+            }
+        }
+
+        return [
+            "view" => VIEW_DIR . "forum/register.php",
+            "meta_description" => "Page de connexion"
+        ];
+    }
     public function login () {}
     public function logout () {}
 }
---
+++ forumPlateau_V2/view/forum/register.php
@@ -0,0 +1,12 @@
+<form action="index.php?ctrl=security&action=register" method="post">
+
+    <input type="mail" name="pseudo" placeholder="Pseudo">
+
+    <input type="text" name="mail" placeholder="Adresse E-mail">
+
+    <input type="password" name="pass1" placeholder="MDP">
+
+    <input type="password" name="pass2" placeholder="Confirmer MDP">
+
+    <input type="submit" value="S'enregistrer">
+</form>
